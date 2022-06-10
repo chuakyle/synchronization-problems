9114ae786f9032363396920e58eeda505e34603e
unknown <kyle_kenneth_chua@dlsu.edu.ph>
2022-06-10 17:13:49 +0800

    added main.py accepts input

---
+++ main.py
@@ -0,0 +1,5 @@
+# Asks and accepts 3 inputs from the user
+
+room_size = input("Enter number of slots inside the fitting room: ")
+blue_threads = input("Enter number of blue threads: ")
+green_threads = input("Enter number of green threads: ")
