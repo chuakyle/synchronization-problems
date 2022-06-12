a347f347c4afc3a2ce3a67ada1d82cb6b0d517db
unknown <kyle_kenneth_chua@dlsu.edu.ph>
2022-06-12 16:27:28 +0800

    added project constraints and polished

--- main.py
+++ main.py
@@ -7,26 +7,34 @@ turn = 0
 # Serves as the number of threads went in critical section, as well as thread ID
 total_counter = 0 
 
-def green(green_threads):
-    global flag, turn, total_counter
 
-    #
+def green(green_threads, room_size):
+    global flag, turn, total_counter
+    
+    
     while(True):
         sleep(0.01)
 
         flag[0] = True
         turn = 1
+        room_counter = 0
         
         while(flag[1] and turn == 1):
             continue
 
         # critical section
-        print("\n--- Green Only ---\n")
-        total_counter += 1
-        green_threads -= 1
+        print("\n<><> Green Only <><>\n")
 
-        print("Thread ID: ", total_counter)
-        print("Color: Green")
+        while(room_counter < room_size):
+            if(green_threads > 0):
+                green_threads -= 1
+                total_counter += 1
+                room_counter += 1
+
+                print("Thread ID: ", total_counter)
+                print("Color: Green\n")
+            else: break
+
         flag[0] = False
 
         # remainder section
@@ -35,25 +43,32 @@ def green(green_threads):
         if(green_threads <= 0):
             break 
 
-def blue(blue_threads):
+def blue(blue_threads, room_size):
     global flag, turn, total_counter
-
+    
+    
     while(True):
         sleep(0.01)
-        
+
         flag[1] = True
-        turn = 0 
+        turn = 0
+        room_counter = 0
 
         while(flag[0] and turn == 0):
             continue
 
         # critical section
-        total_counter += 1
-        blue_threads -= 1
+        print("\n<><> Blue Only <><>\n")
 
-        print("\n--- Blue Only ---\n")
-        print("Thread ID: ", total_counter)
-        print("Color: Blue")
+        while(room_counter < room_size):
+            if(blue_threads > 0):
+                total_counter += 1
+                blue_threads -= 1
+                room_counter += 1
+        
+                print("Thread ID: ", total_counter)
+                print("Color: Blue\n")
+            else: break
 
         flag[1] = False  
 
@@ -62,11 +77,6 @@ def blue(blue_threads):
 
         if(blue_threads <= 0):
             break  
-
-def test(blue_threads):
-    print("Before: ", blue_threads)
-    blue_threads -= 1
-    print("After: ", blue_threads)
     
 # Asks and accepts 3 inputs from the user
 
@@ -78,13 +88,5 @@ room_size = int(room_size)
 blue_threads = int(blue_threads)
 green_threads = int(green_threads)
 
-green = threading.Thread(target = green, args=[green_threads]).start()
-blue = threading.Thread(target = blue, args=[blue_threads]).start()
-
-# blue.start()
-# green.start()
-
-
-#   test = threading.Thread(target = test)
-#   test.start()
-
+green = threading.Thread(target = green, args=[green_threads, room_size]).start()
+blue = threading.Thread(target = blue, args=[blue_threads, room_size]).start()
